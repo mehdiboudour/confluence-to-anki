bf4bec4c55c790d2dde93e32a5a44f8a4a3d2c10
Mehdi BOUDOUR <mehdi@Mehdis-MacBook-Pro.local>
2024-06-17 16:03:02 +0200

    unit test for ResourceAbstractService

---
+++ src/test/java/confluence/services/ResourceAbstractServiceTest.java
@@ -0,0 +1,39 @@
+package confluence.services;
+
+import com.mashape.unirest.http.HttpMethod;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.request.GetRequest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+
+class ResourceAbstractServiceTest {
+
+    private ResourceAbstractService serviceTest;
+
+    @Mock
+    private GetRequest mockGetRequest;
+
+    @BeforeEach
+    void init() {
+        serviceTest = Mockito.mock(
+                ResourceAbstractService.class,
+                Mockito.CALLS_REAL_METHODS
+        );
+        try(MockedStatic<Unirest> mockUnirest = Mockito.mockStatic(Unirest.class)) {
+            mockUnirest.when(() -> Unirest.get(ArgumentMatchers.anyString()))
+                    .thenReturn(mockGetRequest);
+        }
+    }
+
+    @Test
+    void buildAuthorizedGetRequestTest() {
+        GetRequest getRequest = serviceTest.buildAuthorizedGetRequest();
+        Assertions.assertNotNull(getRequest);
+    }
+}
